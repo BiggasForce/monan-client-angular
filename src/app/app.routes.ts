0e4c4ce397c2326c96791a147ac3207253056f3e
Gabriel Lucas <gabriel2014.lucas@outlook.com>
2024-09-14 17:51:06 -0300

    login-13.09

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
+import { TelaPrincipalComponent } from './componentes/tela-principal/tela-principal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'loguei', component: TelaPrincipalComponent}
+];
